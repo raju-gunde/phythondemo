a4b3e1bd022f3ade3a96666bd3565745f2316b92
raju <gunde671@gmail.com>
2025-05-01 19:13:44 +0530

    firstcommit

---
+++ flask_app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+from langchain_community import ChatModels
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Homepage"
+
+@app.route("/home/<username>")
+def home_page(username):
+    return f"Welcome {username}"
+
+@app.route("/age/<int:age_number>")
+def age(age_number):
+    return f"My age is {age_number}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
